e71d6aa127e648835ada65faa45b4b938a0dd4d1
RuslanBTB <87086724+ruslan6677@users.noreply.github.com>
2023-01-21 13:21:15 +0400

    Update info.py

--- info.py
+++ info.py
@@ -6,9 +6,9 @@ id_pattern = re.compile(r'^.\d+$')
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ['API_ID', 10300036])
+API_HASH = environ['API_HASH' "79c295e05c970ddd724f0762ba275104"]
+BOT_TOKEN = environ['BOT_TOKEN', "5905216251:AAFTUm46fM_JEx9tS07VyvS5ZCUMgrYlijg"]
 USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
 
 # Bot settings
@@ -16,16 +16,16 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS', "2124305832"].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS', "-1001502802925"].split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS, "2124305832"', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['DATABASE_NAME']
+DATABASE_URI = environ['DATABASE_URI', "mongodb+srv://Ruslan:Ruslan@cluster0.dees3c1.mongodb.net/?retryWrites=true&w=majority"]
+DATABASE_NAME = environ['DATABASE_NAME', "Cluster0"]
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
